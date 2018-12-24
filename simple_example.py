807874ca13a91f950bfc2e6848ffa04ad52576f6
Dominik Schmid <dominikschmid93@gmail.com>
2018-12-25 02:03:52 +1300

    Parameterize MoveToBall differently.

--- simple_example.py
+++ simple_example.py
@@ -8,8 +8,11 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 class MoveToBall(Exercise):
 
-    def __init__(self, dist_to_ball):
-        self.init_dist_to_ball = dist_to_ball
+    def __init__(self, player_location, player_rotation=Rotator(0, 0, 0), ball_location=Vector3(x=0,y=0,z=100)):
+        self.player_location = player_location
+        self.player_rotation = player_rotation
+        self.ball_location = ball_location
+        self.init_dist_to_ball = None
 
     def get_config_path(self) -> str:
         return join(dirname(__file__), 'rlbot_configs', 'single_soccar.cfg')
@@ -17,14 +20,14 @@ class MoveToBall(Exercise):
     def setup(self, rng: random.Random) -> GameState:
         return GameState(
             ball=BallState(physics=Physics(
-                location=Vector3(x=0, y=0, z=100),
+                location=self.ball_location,
                 velocity=Vector3(0,0,0),
                 angular_velocity=Vector3(0,0,0))),
             cars={
                 0: CarState(
                     physics=Physics(
-                        location=Vector3(x=-self.init_dist_to_ball, y=0, z=0), 
-                        rotation=Rotator(0, 0, 0),
+                        location=self.player_location, 
+                        rotation=self.player_rotation,
                         velocity=Vector3(0, 0, 0),
                         angular_velocity=Vector3(0, 0, 0)),
                     jumped=False,
@@ -36,33 +39,34 @@ class MoveToBall(Exercise):
 
     def on_tick(self, game_tick_packet: GameTickPacket):
         car_pos = game_tick_packet.game_cars[0].physics.location
-        horizontal_dist = math.sqrt(car_pos.x**2 + car_pos.y**2)
-        if horizontal_dist < 0.5 * self.init_dist_to_ball:
+        ball_pos = game_tick_packet.game_ball.physics.location
+        to_ball_x = ball_pos.x - car_pos.x
+        to_ball_y = ball_pos.y - car_pos.y
+        dist_to_ball = math.sqrt(to_ball_x**2 + to_ball_y**2)
+
+        if self.init_dist_to_ball is None:
+            self.init_dist_to_ball = dist_to_ball
+            return
+        if dist_to_ball < 0.5 * self.init_dist_to_ball:
             return Pass()
-        if horizontal_dist > 2.0 * self.init_dist_to_ball:
+        if dist_to_ball > 2.0 * self.init_dist_to_ball:
             return Fail()
         # TODO: timeout
 
 def main():
     result_iter = run_all_exercises({
-        "Moves to ball when close": MoveToBall(500),
-        # "Moves to ball when far": MoveToBall(600),
-        "Z 0": MoveToBall(3000),
-        "Z 1": MoveToBall(3001),
-        "Z 2": MoveToBall(3002),
-        "Z 3": MoveToBall(3003),
-        "Z 4": MoveToBall(3004),
-        "Z 5": MoveToBall(3005),
-        "Z 6": MoveToBall(3006),
-        "Z 7": MoveToBall(3007),
-        "Z 8": MoveToBall(3008),
-        "Z 9": MoveToBall(3009),
+        "00 From side": MoveToBall(Vector3(-3000, 0, 0)),
+        "01 Facing away": MoveToBall(Vector3(1500, 0, 0)),  # This one is expected to fail
+        "02 From own side": MoveToBall(Vector3(0, -4500, 0)),
+        "03 From enemy side": MoveToBall(Vector3(0, 4500, 0)),
+        "04 Medium Distance": MoveToBall(Vector3(0, 2000, 0)),
+        "05 Close": MoveToBall(Vector3(0, 500, 0)),
     })
 
     # Note: results is an iterator, not a list. We can only do this for-loop once.
     num_failed = 0
     for name, result in result_iter:
-        print(f'{name}: {str(result.grade)}')
+        print(f'{name}: {result.grade}')
         if isinstance(result.grade, Fail):
             num_failed += 1
 
