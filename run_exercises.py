c24ba3212a13a0a26a875ab55512db2206d708ab
Dominik Schmid <dominikschmid93@gmail.com>
2018-12-31 02:58:11 +1300

    Added a example of a parameterized Exercise.

--- run_exercises.py
+++ run_exercises.py
@@ -1,15 +1,22 @@
 from os.path import join, dirname
 
-from standard_training.exercises.easy_striker import BallInFrontOfGoal
+from standard_training.exercises.easy_striker import BallInFrontOfGoal, FacingAwayFromBallInFrontOfGoal
 from standard_training.exercises.easy_goalie import BallRollingToGoalie
 from standard_training.exercise_runner import run_exercises
+
+# TODO: playlists.
 
 def run_easy_exercises():
     config_path = join(dirname(__file__), 'rlbot_configs', 'single_soccar.cfg')
     run_exercises({
-        # 'BallInFrontOfGoal': BallInFrontOfGoal(config_path),
+        'Facing ball': BallInFrontOfGoal(config_path),
+        'Facing away from ball 1': FacingAwayFromBallInFrontOfGoal(config_path, 1500.),
+        'Facing away from ball 2': FacingAwayFromBallInFrontOfGoal(config_path, -400.),
+        # 'Facing away from ball 3': FacingAwayFromBallInFrontOfGoal(config_path, 0),
+        'Facing away from opponents goal': FacingAwayFromBallInFrontOfGoal(config_path, 200., car_start_y=5100),
+
         # 'BallInFrontOfGoal2': BallInFrontOfGoal(config_path),
-        'BallRollingToGoalie': BallRollingToGoalie(config_path),
+        # 'BallRollingToGoalie': BallRollingToGoalie(config_path),
         # 'BallRollingToGoalie2': BallRollingToGoalie(config_path),
     }, infinite=True)
 
--- standard_training/exercises/easy_striker.py
+++ standard_training/exercises/easy_striker.py
@@ -28,3 +28,33 @@ class BallInFrontOfGoal(GraderExercise):
 
     def make_grader(self) -> Grader:
         return StrikerGrader()
+
+
+class FacingAwayFromBallInFrontOfGoal(GraderExercise):
+    def __init__(self, config_path, car_start_x, car_start_y=3000):
+        super().__init__(config_path)
+        self.car_start_x = car_start_x
+        self.car_start_y = car_start_y
+
+    def make_game_state(self, rng: random.Random) -> GameState:
+        return GameState(
+            ball=BallState(physics=Physics(
+                location=Vector3(0, 4400, 100),
+                velocity=Vector3(0,0,0),
+                angular_velocity=Vector3(0,0,0))),
+            cars={
+                0: CarState(
+                    physics=Physics(
+                        location=Vector3(self.car_start_x, self.car_start_y, 0),
+                        rotation=Rotator(0, -pi/2, 0),
+                        velocity=Vector3(0, 0, 0),
+                        angular_velocity=Vector3(0, 0, 0)),
+                    jumped=False,
+                    double_jumped=False,
+                    boost_amount=0)
+            },
+            boosts={i: BoostState(0) for i in range(34)},
+        )
+
+    def make_grader(self) -> Grader:
+        return StrikerGrader()
