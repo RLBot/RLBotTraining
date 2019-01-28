bd1b6542604c58355c924af3f3a12daf5bca3b29
Dominik Schmid <dominikschmid93@gmail.com>
2019-01-29 00:24:20 +1100

    Fixed up using the wrong RNG.

--- rlbottraining/common_exercises/easy_striker.py
+++ rlbottraining/common_exercises/easy_striker.py
@@ -1,16 +1,16 @@
 from dataclasses import dataclass, field
 
-import random
 from math import pi
 from pathlib import Path
 
 from rlbot.utils.game_state_util import GameState, BoostState, BallState, CarState, Physics, Vector3, Rotator
 
+from rlbottraining.rng import SeededRandomNumberGenerator
 from rlbottraining.common_exercises.common_base_exercises import StrikerExercise
 
 @dataclass
 class BallInFrontOfGoal(StrikerExercise):
-    def make_game_state(self, rng: random.Random) -> GameState:
+    def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
         return GameState(
             ball=BallState(physics=Physics(
                 location=Vector3(0, 4400, 100),
@@ -19,7 +19,7 @@ class BallInFrontOfGoal(StrikerExercise):
             cars={
                 0: CarState(
                     physics=Physics(
-                        location=Vector3(rng.uniform(-900, 900), 3000, 0),
+                        location=Vector3(900 * rng.n11(), 3000, 0),
                         rotation=Rotator(0, pi / 2, 0),
                         velocity=Vector3(0, 0, 0),
                         angular_velocity=Vector3(0, 0, 0)),
@@ -37,7 +37,7 @@ class FacingAwayFromBallInFrontOfGoal(StrikerExercise):
     car_start_x: float = 0
     car_start_y: float = 2400
 
-    def make_game_state(self, rng: random.Random) -> GameState:
+    def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
         return GameState(
             ball=BallState(physics=Physics(
                 location=Vector3(0, 4400, 100),
@@ -52,7 +52,7 @@ class FacingAwayFromBallInFrontOfGoal(StrikerExercise):
                         angular_velocity=Vector3(0, 0, 0)),
                     jumped=True,
                     double_jumped=True,
-                    boost_amount=0)
+                    boost_amount=20)
             },
             boosts={i: BoostState(0) for i in range(34)},
         )
@@ -60,23 +60,28 @@ class FacingAwayFromBallInFrontOfGoal(StrikerExercise):
 
 # The ball is rolling towards goal but you still need to put it in
 class RollingTowardsGoalShot(StrikerExercise):
-    def make_game_state(self, rng: random.Random) -> GameState:
+    def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
         car_pos = Vector3(0, -2500, 25)
-        ball_pos = Vector3(random.uniform(-1000, 1000), random.uniform(0, 1500), 100)
+        ball_pos = Vector3(1000*rng.n11(), rng.uniform(0, 1500), 100)
         ball_state = BallState(Physics(location=ball_pos, velocity=Vector3(0, 550, 0)))
         car_state = CarState(boost_amount=87, jumped=True, double_jumped=True,
-                             physics=Physics(location=car_pos, rotation=Rotator(0, pi / 2, 0)))
+                             physics=Physics(
+                                location=car_pos,
+                                rotation=Rotator(0, pi / 2, 0),
+                                velocity=Vector3(0, 0, 0),
+                                angular_velocity=Vector3(0, 0, 0),
+                            ))
         enemy_car = CarState(physics=Physics(location=Vector3(0, 5120, 25)))
-        game_state = GameState(ball=ball_state, cars={0: car_state, 1: enemy_car})
+        game_state = GameState(ball=ball_state, cars={0: car_state})
         return game_state
 
 
 def make_default_playlist():
     return [
         BallInFrontOfGoal('Facing ball'),
+        RollingTowardsGoalShot('Rolling Shot'),
         FacingAwayFromBallInFrontOfGoal('Facing directly away from ball', car_start_x=0),
         FacingAwayFromBallInFrontOfGoal('Facing away from ball 1', car_start_x=1500.),
         FacingAwayFromBallInFrontOfGoal('Facing away from ball 2', car_start_x=-400.),
         FacingAwayFromBallInFrontOfGoal('Facing away from opponents goal', car_start_x=200., car_start_y=5100),
-        RollingTowardsGoalShot('Rolling Shot'),
     ]
--- rlbottraining/rng.py
+++ rlbottraining/rng.py
@@ -2,14 +2,11 @@ from random import Random
 
 class SeededRandomNumberGenerator(Random):
     def __init__(self, rng: Random):
+        super().__init__()
         self.setstate(rng.getstate())
 
     def n11(self):
         """
         A Shorthand to get a random value between negative 1 and 1.
         """
-        return self.rng.uniform(-1, 1)
-
-    # TODO: Add some more convenience functions which are RL specific. e.g.
-    #  direction
-    #  angular velocity
+        return self.uniform(-1, 1)
