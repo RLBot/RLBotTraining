eacd5d9730b2bf656dddff2f51dce4b0bc07a119
Dominik Schmid <dominikschmid93@gmail.com>
2019-03-31 21:47:22 +1100

    Added utility to flip teams on an exercise

--- rlbottraining/common_exercises/versus_line_goalie.py
+++ rlbottraining/common_exercises/versus_line_goalie.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 from dataclasses import dataclass, field
 from math import pi
+from typing import Callable
 
 from rlbot.utils.game_state_util import GameState, BoostState, BallState, CarState, Physics, Vector3, Rotator
 from rlbot.matchconfig.match_config import MatchConfig, PlayerConfig, Team
 
 from rlbottraining.common_graders.goal_grader import StrikerGrader
-from rlbottraining.rng import SeededRandomNumberGenerator
-from rlbottraining.training_exercise import Playlist
+from rlbottraining.grading.grader import Grader
 from rlbottraining.match_configs import make_empty_match_config
 from rlbottraining.paths import BotConfigs
+from rlbottraining.rng import SeededRandomNumberGenerator
+from rlbottraining.training_exercise import Playlist
 from rlbottraining.training_exercise import TrainingExercise
 
 
@@ -89,6 +91,15 @@ class SecondShot(VersusLineGoalie):
             boosts={i: BoostState(0) for i in range(34)},
         )
 
+# TODO: move this into a more general file.
+def _mirror_teams(exercise: TrainingExercise, grader_mutator: Callable[[Grader], Grader] = lambda g: g) -> TrainingExercise:
+    from rlbot.utils.game_state_util import flip_y_axis
+    for player in exercise.match_config.player_configs:
+        player.team = 1 - player.team  # 0->1  1->0
+    original_make_game_state = exercise.make_game_state
+    exercise.make_game_state = lambda rng: flip_y_axis(original_make_game_state(rng))
+    exercise.grader = grader_mutator(exercise.grader)
+    return exercise
 
 
 def versus_line_goalie_match_config(attacker: Path, goalie: Path = BotConfigs.line_goalie) -> MatchConfig:
