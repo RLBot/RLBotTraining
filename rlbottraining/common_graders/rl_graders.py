fc769b4abb41a16f8123e36077e8d7d53a445476
Skyborg <fabiorcferreira@gmail.com>
2019-04-14 23:24:05 +0100

    Changed ball touches ground detection
    This is a much improved ball on ground detection that brings it closer to the behaviour of rocket league 
    this allows for touch detection on top of the goal in throwback stadium, and for the ball stop on ground.

--- rlbottraining/common_graders/rl_graders.py
+++ rlbottraining/common_graders/rl_graders.py
@@ -3,6 +3,7 @@
 This module contains graders which mimic the the behaviour of Rocket League custom training.
 """
 
+import math
 
 from dataclasses import dataclass
 from typing import Optional, Mapping, Union
@@ -29,6 +30,16 @@ class RocketLeagueStrikerGrader(CompoundGrader):
         ])
 
 class FailOnBallOnGroundAfterTimeout(FailOnTimeout):
+    def __init__(self, max_duration_seconds):
+        super().__init__(max_duration_seconds)
+        self.previous_ang_x = None
+        self.previous_ang_y = None
+        self.previous_ang_z = None
+
+    def set_previous_angular_velocity(self, ball):
+        self.previous_ang_x = ball.angular_velocity.x
+        self.previous_ang_y = ball.angular_velocity.y
+        self.previous_ang_z = ball.angular_velocity.z
 
     def on_tick(self, tick: TrainingTickPacket) -> Optional[Grade]:
         grade = super().on_tick(tick)
@@ -36,6 +47,28 @@ class FailOnBallOnGroundAfterTimeout(FailOnTimeout):
             return None
         assert isinstance(grade, FailOnTimeout.FailDueToTimeout)
         ball = tick.game_tick_packet.game_ball.physics
-        if ball.location.z < 100 and ball.velocity.z >= 0:
-            return grade
+        hit_ground = False
 
+        if self.previous_ang_z is None:
+            self.set_previous_angular_velocity(ball)
+        else:
+            max_ang_vel = 5.9999601985025075  #Max angular velocity possible
+            previous_ang_norm = math.sqrt(self.previous_ang_x**2 + self.previous_ang_y**2 + self.previous_ang_z**2)
+            if ball.location.z <= 1900:  #Making sure it doesnt count the ceiling
+                if (ball.angular_velocity.x != self.previous_ang_x or ball.angular_velocity.y != self.previous_ang_y):
+                    # If the ball hit anything its angular velocity will change in the x or y axis
+                    if self.previous_ang_z == ball.angular_velocity.z:
+                        # if the z angular velocity did not change it means it was a flat plane.
+                        hit_ground = True
+                    elif previous_ang_norm == max_ang_vel:
+                        # if it was at maximum angular velocity, it may have changed z axis not to exceed max
+                        # fallback on old behaviour
+                        if ball.location.z < 100 and ball.velocity.z >= 0:
+                            hit_ground = True
+        if math.sqrt(self.previous_ang_x**2 + self.previous_ang_y**2 + self.previous_ang_z**2) == 0:
+            # ball is stop on ground, which means it should fail anyway
+            hit_ground = True
+
+        self.set_previous_angular_velocity(ball)
+        if hit_ground:
+            return grade
