74c625e608133a9ee6ad81290ae662073a2ffdbe
Jeroen van Dijk <jeroen11dijk@gmail.com>
2019-10-05 17:45:16 +0200

    Updated silver striker in the same way

--- rlbottraining/common_exercises/silver_striker.py
+++ rlbottraining/common_exercises/silver_striker.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from math import pi
 
+from rlbot.utils.game_state_util import BoostState
 from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator
 
 from rlbottraining.common_exercises.common_base_exercises import StrikerExercise
@@ -13,15 +14,24 @@ class HookShot(StrikerExercise):
     """A shot where you have to hook it to score"""
 
     def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
-        car_pos = Vector3(2000, 500, 25)
-        ball_pos = Vector3(2000, 1000, 100)
-        ball_state = BallState(Physics(location=ball_pos, velocity=Vector3(0, 1000, 0)))
-        car_state = CarState(boost_amount=0, jumped=True, double_jumped=True,
-                             physics=Physics(location=car_pos, velocity=Vector3(0, 1000, 0),
-                                             rotation=Rotator(0, pi / 2, 0)))
-        enemy_car = CarState(physics=Physics(location=Vector3(10000, 10000, 10000)))
-        game_state = GameState(ball=ball_state, cars={0: car_state, 1: enemy_car})
-        return game_state
+        return GameState(
+            ball=BallState(physics=Physics(
+                location=Vector3(2000, 1000, 93),
+                velocity=Vector3(0, 1000, 0),
+                angular_velocity=Vector3(0, 0, 0))),
+            cars={
+                0: CarState(
+                    physics=Physics(
+                        location=Vector3(2000, 500, 25),
+                        rotation=Rotator(0, pi / 2, 0),
+                        velocity=Vector3(0, 1000, 0),
+                        angular_velocity=Vector3(0, 0, 0)),
+                    boost_amount=87),
+                1: CarState(physics=Physics(location=Vector3(10000, 10000, 10000)))
+            },
+            boosts={i: BoostState(0) for i in range(34)},
+        )
+
 
 def make_default_playlist() -> Playlist:
     return [
