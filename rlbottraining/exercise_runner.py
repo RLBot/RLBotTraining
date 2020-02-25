fad2ce8ed98e121e2a46cc0406d3a5aab6ad2a3b
RLMarvin <35970829+RLMarvin@users.noreply.github.com>
2020-02-25 02:45:21 +0100

    Adding documentation and specifying keyword argument.

--- rlbottraining/exercise_runner.py
+++ rlbottraining/exercise_runner.py
@@ -68,9 +68,10 @@ class ReloadPolicy:
 def run_module(python_file_with_playlist: Path, history_dir: Optional[Path] = None,
     reload_policy=ReloadPolicy.EACH_EXERCISE, render_policy=RenderPolicy.DEFAULT):
     """
-    This function repeatedly runs exercises in the module and reloads the module to pick up
-    any new changes to the Exercise. e.g. make_game_state() can be updated or
+    This function repeatedly runs exercises in the module and reloads the module and the agent to pick up
+    any new changes. e.g. make_game_state() can be updated or
     you could implement a new Grader without needing to terminate the training.
+    If the reload_policy is set to ReloadPolicy.NEVER, exercise and the agent will stop reloading on each exercise.
     """
 
     # load the playlist initially, keep trying if we fail
@@ -91,7 +92,7 @@ def run_module(python_file_with_playlist: Path, history_dir: Optional[Path] = No
             playlist = playlist_factory()
             wrapped_exercises = [TrainingExerciseAdapter(ex) for ex in playlist]
             reload_agent = reload_policy != ReloadPolicy.NEVER
-            result_iter = rlbot_run_exercises(setup_manager, wrapped_exercises, seed, reload_agent)
+            result_iter = rlbot_run_exercises(setup_manager, wrapped_exercises, seed, reload_agent=reload_agent)
 
             for i, rlbot_result in enumerate(result_iter):
                 result = ExerciseResult(
